3f3653af46039527786ea661ac04e43b49212fbc
Ege Güngördü <egegungordu@Eges-MacBook-Pro-2.local>
2019-06-27 19:04:55 +0300

    added comments to getprice.py

--- getprice.py
+++ getprice.py
@@ -2,12 +2,12 @@ import bs4
 import requests
 import argparse
 from bs4 import BeautifulSoup
-
+#parse arguments for cli
 parser = argparse.ArgumentParser()
 parser.add_argument("pair",help="The currency pair you wish to search",type=str)
 args = parser.parse_args()
-
+#send get request to yahoo
 url = 'https://finance.yahoo.com/quote/{}%3DX?p={}%3DX'.format(args.pair,args.pair)
 r = requests.get(url)
 soup = bs4.BeautifulSoup(r.text,"xml")
-print(soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text)
+print(soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text)#find currency element from xml
