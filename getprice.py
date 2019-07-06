b137a5ad58cb3f9ee0a680353b58a45252a75708
Ege Güngördü <egegungorduu@hotmail.com>
2019-07-06 23:03:31 +0300

    added amount to transfer (-a) option

--- getprice.py
+++ getprice.py
@@ -7,10 +7,11 @@ def Main():
 	#parse arguments for cli
 	parser = argparse.ArgumentParser()
 	parser.add_argument("pair",help="The currency pair you wish to search",type=str)
+	parser.add_argument("-a", default=1, metavar=("amount"), help="The amount you wish to transfer (defaults to 1)",type=float)
 	args = parser.parse_args()
-	print(getprice(args.pair))
+	print(getprice(args.pair, args.a))
 
-def getprice(currency):
+def getprice(currency, amount):
 	#send get request to yahoo
 	url = 'https://finance.yahoo.com/quote/{}%3DX?p={}%3DX'.format(currency,currency)
 	r = requests.get(url)
@@ -19,8 +20,8 @@ def getprice(currency):
 		#find currency element from xml
 		price = soup.find_all('div', {'class':'My(6px) Pos(r) smartphone_Mt(6px)'})[0].find('span').text
 	except IndexError:
-		return 
-	return price
+		return
+	return float(price) * amount
 
 if(__name__ == "__main__"):
 	Main()
